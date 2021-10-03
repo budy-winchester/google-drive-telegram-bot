6c4f10ae2f0a13f96873d777b26bdabb49170d4b
budy-winchester <88473118+budy-winchester@users.noreply.github.com>
2021-10-04 04:15:32 +0700

    Update gDrive.py

--- bot/helpers/gdrive_utils/gDrive.py
+++ bot/helpers/gdrive_utils/gDrive.py
@@ -23,11 +23,10 @@ logging.getLogger('oauth2client.client').setLevel(logging.ERROR)
 
 
 class GoogleDrive:
-  def shorten_url(link):
+  def shorten_url(url):
     base_url = "http://ouo.io/api/ARlk1o6H?s="
-    Url = self.__G_DRIVE_BASE_DOWNLOAD_URL.format(file_id)
-    link = base_url + Url
-    r = requests.get(link).text
+    url = base_url + url
+    r = requests.get(url).text
 
   def __init__(self, user_id):
     self.__G_DRIVE_DIR_MIME_TYPE = "application/vnd.google-apps.folder"
@@ -165,7 +164,8 @@ class GoogleDrive:
       try:
         uploaded_file = self.__service.files().create(body=body, media_body=media_body, fields='id', supportsTeamDrives=True).execute()
         file_id = uploaded_file.get('id')
-        return Messages.UPLOADED_SUCCESSFULLY.format(filename, shorten_url(link), filesize)
+        url = self.__G_DRIVE_BASE_DOWNLOAD_URL.format(file_id)
+        return Messages.UPLOADED_SUCCESSFULLY.format(filename, shorten_url(url), filesize)
       except HttpError as err:
         if err.resp.get('content-type', '').startswith('application/json'):
           reason = json.loads(err.content).get('error').get('errors')[0].get('reason')

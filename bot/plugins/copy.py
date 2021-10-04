d601e41d4e862c20d516b04a09e283d9f3725de1
budy-winchester <88473118+budy-winchester@users.noreply.github.com>
2021-10-04 12:37:48 +0700

    Update copy.py

--- bot/plugins/copy.py
+++ bot/plugins/copy.py
@@ -3,23 +3,15 @@ from bot.config import BotCommands, Messages
 from bot.helpers.utils import CustomFilters
 from bot.helpers.gdrive_utils import GoogleDrive
 from bot import LOGGER
-import requests
 
 @Client.on_message(filters.private & filters.incoming & filters.command(BotCommands.Clone) & CustomFilters.auth_users)
-def shorten_url(url):
-	base_url = "http://ouo.io/api/ARlk1o6H?s="
-	url = base_url + url
-	print('Original URL', url)
-	r = requests.get(url).text
-
 def _clone(client, message):
   user_id = message.from_user.id
   if len(message.command) > 1:
-    url = message.command[1]
-    LOGGER.info(f'Copy:{user_id}: {url}')
-    sent_message = message.reply_text(Messages.CLONING.format(url), quote=True)
-    link2 = shorten_url(url)
-    msg = GoogleDrive(user_id).clone(link2)
+    link = message.command[1]
+    LOGGER.info(f'Copy:{user_id}: {link}')
+    sent_message = message.reply_text(Messages.CLONING.format(link), quote=True)
+    msg = GoogleDrive(user_id).clone(link)
     sent_message.edit(msg)
   else:
     message.reply_text(Messages.PROVIDE_GDRIVE_URL.format(BotCommands.Clone[0]))
